9aa244b4617da2255c4f385b55224b6d79f69611
Lucas Franco <lucasframoon@gmail.com>
2024-07-12 21:04:40 -0300

    market-2: Add frontend

---
+++ front/src/components/Sales/NewSales.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import BackButton from "../Buttons/BackButton";
+
+function Products() {
+    return (
+        <div className="container">
+            <BackButton />
+            <h1 className="mt-5">Vendas</h1>
+            <table className="table table-striped mt-3">
+                <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Data</th>
+                    <th>Total</th>
+                    <th>Produtos</th>
+                </tr>
+                </thead>
+                <tbody>
+                {/* TODO sales */}
+                <tr>
+                    <td>1</td>
+                    <td>12/07/2024</td>
+                    <td>R$ 100,00</td>
+                    <td>Produto1, Produto2</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default Products;
