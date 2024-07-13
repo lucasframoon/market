c402cd690bdb1a6e4fe1109418a87bad2ed5b4d6
Lucas Franco <lucasframoon@gmail.com>
2024-07-13 16:54:21 -0300

    market-3: Adjust cors

--- Api/public/index.php
+++ Api/public/index.php
@@ -1,5 +1,12 @@
 <?php
-header('Access-Control-Allow-Origin: http://localhost:3000');
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
 
 require '../vendor/autoload.php';
 require '../config/diconfig.php';
