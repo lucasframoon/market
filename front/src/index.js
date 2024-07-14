9fa2968eda304b978c3cb03af1b040b3e4495f6f
Lucas Franco <lucasframoon@gmail.com>
2024-07-14 15:28:18 -0300

    market-3: Add product form

--- front/src/index.js
+++ front/src/index.js
@@ -1,40 +1,45 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import {createRoot} from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import Dashboard from "./components/Dashboard/Dashboard";
 import Products from "./components/Products/Products";
+import ProductForm from "./components/Products/ProductForm";
 import ProductTypes from "./components/ProductTypes/ProductTypes";
+import ProductTypeForm from "./components/ProductTypes/ProductTypeForm";
 import Sales from "./components/Sales/SalesList";
 import NewSales from "./components/Sales/NewSales";
-import ProductTypeForm from "./components/ProductTypes/ProductTypeForm";
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <NewSales />
+        element: <NewSales/>
     },
     {
         path: "/dashboard",
-        element: <Dashboard />
+        element: <Dashboard/>
     },
     {
         path: "/products",
-        element: <Products />
+        element: <Products/>
+    },
+    {
+        path: "/product/form/:id?",
+        element: <ProductForm/>
     },
     {
         path: "/product-types",
-        element: <ProductTypes />
+        element: <ProductTypes/>
     },
     {
         path: "/product-types/form/:id?",
-        element: <ProductTypeForm />
+        element: <ProductTypeForm/>
     },
     {
         path: "/sales",
-        element: <Sales />
+        element: <Sales/>
     }
 ])
 
@@ -42,7 +47,7 @@ const root = createRoot(document.getElementById('root'));
 
 root.render(
     <React.StrictMode>
-        <RouterProvider router={router} />
+        <RouterProvider router={router}/>
     </React.StrictMode>
 );
 
